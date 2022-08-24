5ea68496f15e36d7678a3cd401f5d30cbd5209dc
Stoyann <stoyann.openclassrooms@gmail.com>
2022-08-25 10:15:54 +1100

    'message'

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -4,19 +4,41 @@ import Spinner from './Spinner.svelte';
 
 	const dispatch = createEventDispatcher();
 	let recherche: string = '';
+	let message: string = '';
 
-	// Et pour l'utiliser
 
+
+	
+	let toggle = false;
+
+	// open /close modal
+	function toggleClass() {
+		toggle = !toggle;
+	}
 	const onSubmit = () => {
 		if (recherche.length >= 3) {
+			message = ""
 			setTimeout(() => {
 				dispatch('recherche-user', { txt: recherche });
 			}, 2000);
+		} else {
+			toggle =true
+			message = "Votre recherche doit être composée d'un minimum de 3 caractères."
 		}
 	};
+	
+
 </script>
 
 <form on:submit|preventDefault={onSubmit}>
+	<div class=" {toggle ? 'notification  is-danger p-2 mb-5  ' : ''}">
+	
+	{#if toggle}
+		
+	<button on:click={toggleClass} class="delete"></button>
+	{message}
+	{/if}
+	  </div>
 	<div class="field">
 		<p class="control has-icons-left ">
 			<input bind:value={recherche} class="input is-rounded " type="text" placeholder="Recherche" />
@@ -24,5 +46,7 @@ import Spinner from './Spinner.svelte';
 				<i class="fa-solid fa-magnifying-glass" />
 			</span>
 		</p>
+
 	</div>
 </form>
+
--- src/components/UserCard.svelte
+++ src/components/UserCard.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-    	export /**
+	export /**
 	 * @type {{  firstName: string; lastName: string; id: number;  }}
-	 */ let item:any
+	 */ let item: any;
 
-     let toggle = false
+	let toggle = false;
 
-function toggleClass() {
-    toggle = !toggle
-}
+	// open /close modal
+	function toggleClass() {
+		toggle = !toggle;
+	}
 
 	/**
 	 * formate name
@@ -17,7 +18,7 @@ function toggleClass() {
 	 *
 	 * @returns formated name
 	 */
-	function formateName(firstName:string, lastName: string): string {
+	function formateName(firstName: string, lastName: string): string {
 		let last = lastName.toUpperCase();
 		let first = firstName.charAt(0).toUpperCase() + firstName.slice(1);
 		let formatName = `${first + ' ' + last}`;
@@ -25,49 +26,58 @@ function toggleClass() {
 	}
 </script>
 
-<button on:click ={toggleClass} class="button is-white is-small"><i class="fa-solid fa-ellipsis" /></button>
-
+<button on:click={toggleClass} class="button is-white is-small"
+	><i class="fa-solid fa-ellipsis" /></button
+>
 
 <div class="modal p-5 {toggle ? 'is-active' : ' '} ">
-    <div on:click ={toggleClass} class="modal-background"/>
+	<div on:click={toggleClass} class="modal-background" />
 	<div class="modal-card">
 		<header class="modal-card-head">
-		
-			<button on:click ={toggleClass} class="delete" aria-label="close" />
+			<button on:click={toggleClass} class="delete" aria-label="close" />
 		</header>
 		<section class="modal-card-body">
 			<!-- Content ... -->
-            <div class="top">
-                <figure class="image is-110x110">
-                    <img src={item.image} alt="">
-                  </figure>
-                <div class="contact ">
-                    <h3 class="title is-2">{formateName(item.firstName , item.lastName)}</h3>
-                    <p  class="title is-5 m-2"><i class=" mr-4 fa-solid fa-phone"></i> {item.phone}</p>
-                    <p  class="title is-5"><i class=" mr-4 fa-solid fa-envelope"></i> {item.email}</p>
-                </div>
-            </div>
-            <div class="content m-5">
-              <div class="name mb-5">
-
-                  <div class="title is-6">Nom:  <span class=" tag is-dark is-medium" >{item.lastName}</span></div>
-                  <div class="title is-6">Prénom:  <span class=" tag is-dark is-medium" >{item.firstName}</span></div>
-                </div>
-              <div class="info mb-5">
-
-                  <div class="title is-6 m-1 ">Date de naissance:  <span class=" tag is-dark is-medium" >{ new Date(item.birthDate).toLocaleDateString() }</span></div>
-                  <div class="title is-6 m-1">Sexe:  <span class=" tag is-dark is-medium" >{item.gender.slice(0, 1).toUpperCase()}</span></div>
-                </div>
-              <div class="info mb-5">
-
-                  <div class="title is-6 m-1">SSN:  <span class="tag is-dark is-medium" >{item.ssn }</span></div>
-                  <div class="title is-6 m-1">IBAN: <span class=" tag is-dark is-medium" >{item.bank.iban}</span></div>
-                </div>
-              
-            </div>
-        
-            
+			<div class="top">
+				<figure class="image is-110x110">
+					<img src={item.image} alt="" />
+				</figure>
+				<div class="contact ">
+					<h3 class="title is-2">{formateName(item.firstName, item.lastName)}</h3>
+					<p class="title is-5 m-2"><i class=" mr-4 fa-solid fa-phone" /> {item.phone}</p>
+					<p class="title is-5"><i class=" mr-4 fa-solid fa-envelope" /> {item.email}</p>
+				</div>
+			</div>
+			<div class="content m-5">
+				<div class="name mb-5">
+					<div class="title is-6">
+						Nom: <span class=" tag is-dark is-medium">{item.lastName}</span>
+					</div>
+					<div class="title is-6">
+						Prénom: <span class=" tag is-dark is-medium">{item.firstName}</span>
+					</div>
+				</div>
+				<div class="info mb-5">
+					<div class="title is-6 m-1 ">
+						Date de naissance: <span class=" tag is-dark is-medium"
+							>{new Date(item.birthDate).toLocaleDateString()}</span
+						>
+					</div>
+					<div class="title is-6 m-1">
+						Sexe: <span class=" tag is-dark is-medium">{item.gender.slice(0, 1).toUpperCase()}</span
+						>
+					</div>
+				</div>
+				<div class="info mb-5">
+					<div class="title is-6 m-1">
+						SSN: <span class="tag is-dark is-medium">{item.ssn}</span>
+					</div>
+					<div class="title is-6 m-1">
+						IBAN: <span class=" tag is-dark is-medium">{item.bank.iban}</span>
+					</div>
+				</div>
+			</div>
 		</section>
 		<footer class="modal-card-foot" />
 	</div>
-  </div>
+</div>
