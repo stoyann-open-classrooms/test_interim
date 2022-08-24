17857b25e21246b04ebfe758a7e39f37d3c0962a
Stoyann <stoyann.openclassrooms@gmail.com>
2022-08-24 22:55:52 +1100

    test store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+  import storeData from '../store/store'
 	import Header from '../components/Header.svelte';
 	import Footer from '../components/Footer.svelte';
 	import SearchBar from '../components/SearchBar.svelte';
 	import UserList from '../components/UserList.svelte';
 	import '../app.scss';
+  let txt:string
   let users: any[]|undefined = []
   function  fetchUsers() {
     fetch('https://dummyjson.com/users')
@@ -14,12 +16,15 @@
     })
   }
   fetchUsers()
-  
+  storeData.subscribe(valeur => {
+    txt = valeur
+  })
 	
 </script>
 
 <Header />
 <main class="container p-5">
+  <p>{txt}</p>
 	<SearchBar />
 	<UserList {users}/>
 </main>
---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { writable } from "svelte/store";
+
+let txt = writable('hello depuis le  store')
+  
+ 
+
+export default txt
