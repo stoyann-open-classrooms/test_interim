db8a9512a55b5ec7fbe0bcce840f0e14ca5b2bce
Stoyann <stoyann.openclassrooms@gmail.com>
2022-08-25 01:08:02 +1100

    favicon

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,6 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
 });
+
+
+// TODO = tester le formatage du nom et prenom
