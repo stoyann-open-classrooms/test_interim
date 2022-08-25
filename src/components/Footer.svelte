62b9831471ab96ef9f2be0736c5f3ea44a9b5225
Stoyann <stoyann.openclassrooms@gmail.com>
2022-08-25 13:20:08 +1100

    footer link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,21 @@
-<footer class="footer">
+
+  <footer class="footer">
     <div class="content has-text-centered">
+      <div class="m-3">
+        
+          <a class="m-4" href="/about"> <i class=" mr-2 fa-solid fa-comment-dots"></i> A propos</a>
+        
+        
+          <a class="m-4" href="/documentation">  <i class=" mr-2 fa fa-book"></i> Documentation</a>
+        
+        
+          <a class="m-4" href="/bug-report">  <i class=" mr-2 fa  fa-bug"></i>  Signaler un problème</a>
+    
+      </div>
       <p>
-        <strong>Interim +</strong> 
+        <strong>Développer
+        </strong> par <a href="https://github.com/stoyann-open-classrooms">Stoyann VELTEN</a>.
+   
       </p>
     </div>
   </footer>
