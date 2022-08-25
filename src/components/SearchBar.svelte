061c2a71bd11a19bd284f44fecb2e6f757fb1452
Stoyann <stoyann.openclassrooms@gmail.com>
2022-08-25 11:16:57 +1100

    error message request

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -7,19 +7,29 @@
 	let message: string = '';
 
 	let toggle = false;
-
+	let error = 0
 	// open /close modal
 	function toggleClass() {
 		toggle = !toggle;
 	}
 	function clear() {
-		recherche =''
+		recherche = '';
 	}
 	const onSubmit = () => {
+		error++
 		if (recherche.length >= 3) {
 			message = '';
+  console.log(error);
+  
 			setTimeout(() => {
-				dispatch('recherche-user', { txt: recherche });
+				if ( error === 4) {
+
+					toggle = true;
+					message = "Une erreur est survenue, merci de réessayer.";
+					error = 0
+				} else {
+					dispatch('recherche-user', { txt: recherche });
+				}
 			}, 2000);
 		} else {
 			toggle = true;
@@ -31,24 +41,22 @@
 <form on:submit|preventDefault={onSubmit}>
 	<div class=" {toggle ? 'notification  is-danger p-2 mb-5  ' : ''}">
 		{#if toggle}
-			<button on:click={toggleClass} class="delete" />
+			<button on:click={toggleClass} on:focus={clear} class="delete" />
 			{message}
 		{/if}
 	</div>
 	<div class="field">
 		<p class="control has-icons-left has-icons-right ">
-		
 			<input bind:value={recherche} class="input is-rounded " type="text" placeholder="Recherche" />
 			<span class="icon is-small is-left">
 				<i class="fa-solid fa-magnifying-glass" />
 			</span>
-			{#if recherche.length > 0 }
-			
-			<span class="icon is-small is-right">
-				<button on:focus={clear} class="delete">
-					<i class="fa-solid fa-xmark" />
-				</button>
-			</span>
+			{#if recherche.length > 0}
+				<span class="icon is-small is-right">
+					<button on:focus={clear} class="delete">
+						<i class="fa-solid fa-xmark" />
+					</button>
+				</span>
 			{/if}
 		</p>
 	</div>
